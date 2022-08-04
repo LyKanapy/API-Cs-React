2b6888b5ee6c20a1afb2318205762b7e96f08ebe
BigToeEnergy <l.kanapickas@gmail.com>
2022-08-04 17:05:41 +0300

    S16

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -16,9 +16,10 @@ namespace Persistence
 
         public  DbSet<Activity> Activities { get; set; }
         public  DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
-        {
+        { 
             base.OnModelCreating(builder);
 
             builder.Entity<ActivityAttendee>(x=> x.HasKey(aa=> new {aa.AppUserId, aa.ActivityId} ));
